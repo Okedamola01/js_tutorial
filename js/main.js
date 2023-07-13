de89c5bd10a0321fe2792ebc7a6c151fb8c12632
Damoke01 <okedamola01@gmail.com>
2023-07-13 02:50:43 +0100

    Updated

--- js/main.js
+++ js/main.js
@@ -21,6 +21,7 @@ console.log(Math.random());
 console.log(Math.random());
 console.log(Math.random());
 console.log(Math.random());
+console.log(2+2);
 console.log(Math.floor(Math.random() * 10 + 1));
 console.log(Math.floor(Math.random() * 10 + 1));
 console.log(Math.floor(Math.random() * 10 + 1));
